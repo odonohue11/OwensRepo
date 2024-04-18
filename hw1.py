7cd1264a5b6b0ee5e4badca633340d0cdf3bc195
odonohue11 <166739431+odonohue11@users.noreply.github.com>
2024-04-18 10:14:12 -0400

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,115 @@
+# Owen Donohue
+# Cite any sources you used to help with the homework including TAs and classmates
+
+def string3(string):
+    """
+    Given a string, return a new string made of 3 copies of the last 2 chars of the original string.
+    The string length will be at least 2.
+    """
+    return string[-2:] * 3
+
+def has123(nums):
+    """
+    Given an list of ints, return True if the sequence of numbers
+    1, 2, 3 appears in the list somewhere. 1,2,3 must occur in order.
+    """
+    for i in range(len(nums) - 2):
+        if nums[i] == 1 and nums[i + 1] == 2 and nums[i + 2] == 3:
+            return True 
+    return False 
+
+def hascode(string):
+    """
+    Return the number of times that the string "code" appears anywhere in the given string,
+    except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+    """
+    count = 0
+    for i in range(len(string) - 3):
+       if string[i:i + 2] == "co" and string[i + 3] == "e":
+           count += 1
+    return count
+
+def samecatdog(string):
+    """
+    Return True if the string "cat" and "dog" appear the same number of times in the given string.
+   *** This can be simplfied using a Python string method ***
+    """
+    countDog = 0
+    countCat = 0
+    for i in range(len(string)-2):
+        if string[i] == "d" and string[i + 1] == "o" and string[i + 2] == "g":
+            countDog += 1
+        if string[i] == "c" and string[i + 1] == "a" and string[i + 2] == "t":
+            countCat += 1
+    if countCat == countDog:
+        return True
+    else:
+        return False
+
+def centered_avg(nums):
+    """
+    Return the "centered" average of a list of ints, which we'll say is the mean average of the
+    values, except ignoring the largest and smallest values in the list. If there are
+    multiple copies of the smallest value, use just one copy, and likewise for the largest value.
+    Use floor division to produce the final average. You may assume that the list is length 3 or more.
+    """
+    max_index = 0
+    min_index = 0
+    for i in range(1,len(nums)):
+        if nums[i] < nums[min_index]:
+            min_index = i
+        if nums[i] > nums[max_index]:
+            max_index = i  
+                
+    temp = nums[min_index]
+    nums[min_index] = nums[0]
+    nums[0] = temp
+
+    temp2 = nums[max_index]
+    nums[max_index] = nums[-1]
+    nums[-1] = temp2
+
+    total = 0
+    for i in range(1,len(nums)-1):
+        total = nums[i] + total
+    mean = total // (len(nums)-2)
+    return mean
+
+# Test functions
+assert string3("Hello") == 'lololo', 'string3(Hello) expected lololo'
+print("correct")
+assert string3("ab") == 'ababab', 'string3(ab) expected ababab'
+print("correct")
+assert string3("Hi") == 'HiHiHi', 'string3(Hi) expected HiHiHi'
+print("correct")
+
+assert has123([1, 1, 2, 3, 1]) is True, '[1, 1, 2, 3, 1] has 123'
+print("correct")
+assert has123([1, 1, 2, 4, 1, 3]) is False, '[1, 1, 2, 3, 1] does not have sequence 123'
+print("correct")
+assert has123([1, 1, 2, 1, 2, 3]) is True, '[1, 1, 2, 1, 2, 3] has 123'
+print("correct")
+
+assert hascode("aaacodebbb") == 1, 'aaacodebbb has code'
+print("correct")
+assert hascode("aaacodebbb") == 1, 'codexxcode has code'
+print("correct")
+assert hascode("cozexxcope") == 2, 'cozexxcope has code'
+print("correct")
+
+assert samecatdog("catdog") is True, 'catdog appear same number of times'
+print("correct")
+assert samecatdog("catcat") is False, 'catcat does not appear same number of times'
+print("correct")
+assert samecatdog("1cat1cadodog") is True, '1cat1cadodog appear the same number of times'
+print("correct")
+
+assert centered_avg([1, 2, 3, 4, 100]) == 3, 'average [1, 2, 3, 4, 100] is 3'
+print("correct")
+assert centered_avg([1, 1, 5, 5, 10, 8, 7]) == 5, 'average [1, 1, 5, 5, 10, 8, 7] is 5'
+print("correct")
+assert centered_avg([-10, -4, -2, -4, -2, 0]) == -3, 'average [-10, -4, -2, -4, -2, 0] is -3'
+print("correct")
+
+
+# Problems found on https://codingbat.com/python
